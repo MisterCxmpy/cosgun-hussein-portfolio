a3d58d08074ec424325ab024f154a522c38da933
MisterCxmpy <cosgunhussein@gmail.com>
2023-10-06 16:30:20 +0100

    changed spelling of amateur because it was wrong and this person called ana kept nagging me about me

--- src/sections/Hero/index.jsx
+++ src/sections/Hero/index.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
       <article className={styles['hero-content']}>
         <h1 className={styles['text']}>Hi, my name is</h1>
         <h2 className={styles['text']}>Cosgun Hussein</h2>
-        <h3 className={styles['text']}>Amatuer Web Developer</h3>
+        <h3 className={styles['text']}>Amateur Web Developer</h3>
         <p className={styles['text']}>Welcome to my website! I'm passionate about creating beautiful and functional websites. Feel free to explore and get to know more about my work.</p>
       </article>
       <Wheel />
